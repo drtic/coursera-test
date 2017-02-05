d76c320e7138d83fe8a577433e2f72b3788ed15a
jaro <jaro@d>
2017-02-04 21:39:53 -0800

    watchers manual setup

--- sandbox/app.js
+++ sandbox/app.js
@@ -7,17 +7,29 @@
   CounterController.$inject = ['$scope'];
   function CounterController($scope){
     $scope.onceCounter = 0;
+    $scope.counter = 0;
+
     $scope.showNumberOfWatchers = function() {
           console.log("Number of Watchers: ", $scope.$$watchersCount );
     };
 
     $scope.countOnce = function () {
       $scope.onceCounter = 1;
+      console.log("once function");
+    };
+
+    $scope.upCounter = function () {
+      $scope.counter++;
     };
 
     $scope.$watch('onceCounter', function (newValue, oldValue){
-      console.log("oldValue: ", oldValue);
-      console.log("newValue: ", newValue);
+      console.log("onceCounter oldValue: ", oldValue);
+      console.log("onceCounter newValue: ", newValue);
+    });
+
+    $scope.$watch('counter', function (newValue, oldValue){
+      console.log("counter oldValue: ", oldValue);
+      console.log("counter newValue: ", newValue);
     });
 
   };
