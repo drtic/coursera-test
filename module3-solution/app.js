982e021ba5053268785e8229b349275bd30a2720
jaro <jaro@dulina.net>
2017-02-15 11:25:24 -0800

    WIP searchable menu

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -1,36 +1,61 @@
-(function(){
+(function() {
   'use strict';
 
-  angular.module('',[])
-  .controller('', )
-  .service('', )
+  angular.module('NarrowItDownApp',[])
+  .controller('NarrowItDownController', NarrowItDownController)
+  .service('MenuSearchService', MenuSearchService)
   .constant('ApiBasePath', "http://davids-restorant.herokuapp.com");
 
-  MenuCategoriesController.$inject = ['MenuCategoriesService'];
-  function MenuCategoriesController(MenuCategoriesService) {
-    var menu = this;
+  NarrowItDownController.$inject = ['MenuSearchService'];
+  function NarrowItDownController(MenuSearchService) {
+    var narrowit = this;
 
-    var promise = MenuCategoriesService.getMenuCategories();
-
-    promise.then({
-      menu.categories = response.data;
-    })
-    .catch(function(error) {
-      console.log("Error 1");
-    });
-  }
-
-
-  MenuGetLIst = ['$http', 'ApiBasePath']
-  function MenuGetLIst = function($http, ApiBasePath) {
-    var service = this;
-
-    service.getMenuCategories = function() {
-      var response = $http({
-        method: "GET",
-        url: ( ApiBasePath + "/categories.json")
-      });
-      return response;
+    narrowit.fn1 = function() {
+      console.log(MenuSearchService.getMatchedMenuItems('------'));
     };
   };
+
+  MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+  function MenuSearchService($http, ApiBasePath) {
+    var service = this;
+
+    service.getMatchedMenuItems = function getMatchedMenuItems(searchTerm) {
+      return searchTerm + "!";
+    };
+    // function getMatchedMenuItems(searchTerm)
+    // {
+    //
+    // };
+
+  };
+
+
+
+  // MenuCategoriesController.$inject = ['MenuCategoriesService'];
+  // function MenuCategoriesController(MenuCategoriesService) {
+  //   var menu = this;
+  //
+  //   var promise = MenuCategoriesService.getMenuCategories();
+  //
+  //   promise.then({
+  //     menu.categories = response.data;
+  //   })
+  //   .catch(function(error) {
+  //     console.log("Error 1");
+  //   });
+  // }
+
+
+  // MenuGetLIst = ['$http', 'ApiBasePath']
+  // function getMatchedMenuItems = function($http, ApiBasePath) {
+  //   var service = this;
+  //
+  //   service.getMenuCategories = function() {
+  //     var response = $http({
+  //       method: "GET",
+  //       url: ( ApiBasePath + "/categories.json")
+  //     });
+  //     return response;
+  //   };
+  // };
 })();
