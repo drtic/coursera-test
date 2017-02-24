b9b55bc7bab1162b94b9dfcb06b9bdc67c3adac1
jaro <jaro@dulina.net>
2017-02-23 18:58:47 -0800

    WIP from fre

---
+++ module4-solution/js/categories.component.js
@@ -0,0 +1,12 @@
+(function(){
+  'use strict';
+
+  angular.module('cateogories', [])
+  .controller('cateogoriesController', cateogoriesController)
+  ;
+
+  function cateogoriesController(){
+    
+  };
+
+})();
---
+++ module4-solution/js/menuapp.module.js
@@ -0,0 +1,13 @@
+(function(){
+    'use strict';
+
+    angular.module('menuapp', ['cateogories'])
+    .controller('menucontroller', menucontroller )
+    ;
+
+    function menucontroller() {
+
+    };
+
+})();
+an
