453d5850a5ba6cff235b9c11ce1318b0c76db1b2
jaro <jaro@dulina.net>
2017-02-24 22:13:03 -0800

    last changes

--- module4-solution/js/routes.js
+++ module4-solution/js/routes.js
@@ -3,8 +3,12 @@
     .config(RoutesConfig);
 
     RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
-    function RoutesConfig($stateProvider, urlRouterProvider) {
+    function RoutesConfig($stateProvider, $urlRouterProvider) {
 
+      //Redirect to tab 1 if no other URL matches
+      $urlRouterProvider.otherwise('/tab1');
+
+      //Set up UI states
     };
 
 })();
